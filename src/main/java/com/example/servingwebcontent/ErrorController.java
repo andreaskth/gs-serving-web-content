a88a5a59a9e9009251902f694b2475a6eba2ea53
Andreas K <karrby@kth.se>
2021-04-29 16:02:33 +0200

    Change indentation in ErrorController to tabs instead of spaces

--- src/main/java/com/example/servingwebcontent/ErrorController.java
+++ src/main/java/com/example/servingwebcontent/ErrorController.java
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ErrorController {
 
-    private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
+	private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
 
 	@GetMapping("/generate-errors")
 	public String generateErrors(@RequestParam(name="count", required=false, defaultValue="10") String count, Model model) {
-        int nrOfErrors = Integer.parseInt(count);
+		int nrOfErrors = Integer.parseInt(count);
 
-        for (int i = 0; i < nrOfErrors; i++) {
-            logger.error("Error generated in /generate-errors: " + i);
-        }
+		for (int i = 0; i < nrOfErrors; i++) {
+			logger.error("Error generated in /generate-errors: " + i);
+		}
 
 		model.addAttribute("count", count);
 		return "error";
